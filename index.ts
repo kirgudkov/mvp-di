bf2ecbec32acaee34dc7f24ac8cd305f423c7906
Kirill Gudkov <for.example.nickname@gmail.com>
2018-12-31 01:18:13 +0300

    append Presenter export

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export {inject} from './src/Inject';
 export {bind} from './src/Bind';
 export {injectedProperty} from './src/InjectedProperty';
 export {DIBuilder} from './src/DIBuilder';
+export {Presenter} from './src/Presenter';
