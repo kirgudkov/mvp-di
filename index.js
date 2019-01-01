4467f7647d89c736f4c58fcd235489a3133f7499
Kirill Gudkov <for.example.nickname@gmail.com>
2019-01-01 23:01:42 +0300

    вроде как наладил экспорты

---
+++ index.js
@@ -0,0 +1,5 @@
+export {inject} from './src/Inject';
+export {bind} from './src/Bind';
+export {injectedProperty} from './src/InjectedProperty';
+export {DIBuilder} from './src/DIBuilder';
+export {Presenter} from './src/Presenter';
