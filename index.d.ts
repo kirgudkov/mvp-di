b619b32e512ee141411e3c595258195e5d31032c
Kirill Gudkov <for.example.nickname@gmail.com>
2019-01-01 22:09:34 +0300

    Попробовал накинуть d.ts файл

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare module 'presenter-injection' {
+  export function bind<T extends Function>(target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any>;
+  export function injectedProperty(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any>;
+  export function inject<T extends Object>(target: Object, propertyKey: string): any;
+
+  export class Injectable extends Function {}
+  export class PresenterFactory {
+    create<T>(type: string, view: Object): T;
+  }
+  export class Presenter<T> {
+    protected view: T;
+    constructor(view: T);
+  }
+  export class DIBuilder {
+    static types: Object;
+    static build(types: Object): void;
+  }
+}
--- index.ts
+++
@@ -1,5 +0,0 @@
-export {inject} from './src/Inject';
-export {bind} from './src/Bind';
-export {injectedProperty} from './src/InjectedProperty';
-export {DIBuilder} from './src/DIBuilder';
-export {Presenter} from './src/Presenter';
