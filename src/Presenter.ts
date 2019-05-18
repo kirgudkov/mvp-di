e407bdf847ed2fd915116f0bde6a5e04c6bca680
Kirill Gudkov <for.example.nickname@gmail.com>
2019-05-18 12:48:36 +0300

    Update Presenter.ts

--- src/Presenter.ts
+++ src/Presenter.ts
@@ -1,7 +1,15 @@
+import { Param } from 'mvp-di';
+
 export class Presenter<T> {
+  [key: string]: any
+
   protected view!: T;
 
   constructor(view: T) {
     this.view = view;
+  }
+
+  lateInit(...params: Param[]) {
+    params.forEach(param => this[Object.keys(param)[0]] = param[Object.keys(param)[0]]);
   }
 }
